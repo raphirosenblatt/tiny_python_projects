0011a511fa4965c3aee237fc6c8307318f4d2605
Raphi Rosenblatt <67854440+raphirosenblatt@users.noreply.github.com>
2020-08-04 16:11:24 -0700

    Added shebang line

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,2 +1,5 @@
+#!/usr/bin/env python3
+#The 'shebang' above tells the program what to use to execute itpyth
+#We made this program executable by typing into the terminal: chmod +x hello.py
 #say hello
 print("Hello, World!")
